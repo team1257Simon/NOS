fda885111a8bf1e1c29ea54a35838eb58633e32a
team1257Simon <nsimon@ucvts.tec.nj.us>
2013-09-17 08:45:57 -0400

    Update ncio.h

--- ncio.h
+++ ncio.h
@@ -3,7 +3,7 @@
 void nc_tprintf(char*);
 void nc_tscanf(char*);
 void nc_tinit();
-char inb(unsigned int port);
-void outb(unsigned int port, unsigned char val);
+inline char inb(unsigned int port);
+inline void outb(unsigned int port, unsigned char val);
 #define nprintf(q) nc_tprintf(q)
 #define nscanf(q) nc_tscanf(q)
