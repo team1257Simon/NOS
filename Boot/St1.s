d294ff0f2a28cdb0f31c1d0a606fcec12853800b
team1257Simon <nsimon@ucvts.tec.nj.us>
2013-09-19 04:36:10 -0700

    Thry - Commited with Jackalope (http://jackalope.me)

--- Boot/St1.s
+++ Boot/St1.s
@@ -1,15 +1,18 @@
-.386
-extrn _stage2_main:0x1000h
-.text
+
+SECTION .text
 bst:
 cli
+push ax
+mov al,0xdd
+out 0x64,al
+pop ax
 mov ax,cs
 mov ds,ax
 mov es,ax
 mov ss,ax
-mov ebp,0x7c00h
-mov esp,0x7c00h
+mov ebp,0x7c00
+mov esp,0x7c00
 sti
-call _stage2_main
+jmp 0x1000
 .rept:
 jmp .rept
